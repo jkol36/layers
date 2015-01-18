c9a6decf069f2eedd124c60526f56c27097bc781
jkol36 <jonathankolman@gmail.com>
2015-01-18 18:07:35 -0500

    updated production database to postgresql

--- layers/settings.py
+++ layers/settings.py
@@ -61,14 +61,24 @@ WSGI_APPLICATION = 'layers.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'layers_db',
+        'USER': 'layers_43',
+        'PASSWORD':'StoreTheLayers',
+        'HOST':'localhost',
+        'PORT':'',
+        }
+    }
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
