2f294269d3b07f764b58170d1c94476865105c12
jkol36 <jonathankolman@gmail.com>
2015-01-28 10:00:49 -0500

    Render index.jade with error codes if signup_for_emails fails

--- layers/subscribe/views.py
+++ layers/subscribe/views.py
@@ -12,8 +12,9 @@ def signup_for_emails(request):
 	
 	form = signup_for_emails_form(request.POST)
 	if not form.is_valid():
-		messages.error(request, t + z.as_text() for t, z in form.errors)
-
+		for t, z in form.errors:
+			messages.error(request, t + z.as_text())
+		return render(request, 'index.jade')
 	form.save()
 	messages.success(request, 'You have signed up for emails!')
 	return redirect('home')
