59165bae1b604d2738905ef773194c81548aaf66
jkol36 <jonathankolman@gmail.com>
2015-02-02 16:11:41 -0500

    changed client and designer fields from ForeignKey to OneToOneField

--- layers/settings.py
+++ layers/settings.py
@@ -63,18 +63,18 @@ WSGI_APPLICATION = 'layers.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-if DEBUG == True:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+#if DEBUG == True:
+  #  DATABASES = {
+      #  'default': {
+       #     'ENGINE': 'django.db.backends.sqlite3',
+        #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #}
+    #}
 else:
     DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'43_layers',
+        'NAME':'django_db',
         'USER': 'layers_django',
         'PASSWORD':'StoreTheLayers',
         'HOST':'localhost',
