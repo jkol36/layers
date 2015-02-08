b918411f9d4e4a936cf134d704354df79212c02f
jkol36 <jonathankolman@gmail.com>
2015-02-08 18:34:07 -0500

    added mailchimp

--- layers/projects/models.py
+++ layers/projects/models.py
@@ -7,6 +7,7 @@ from sorl.thumbnail import ImageField
 class Project(models.Model):
 	STATUS_CHOICES = (
 		('submit_idea', 'Submit Idea'),
+		('assigning_designer', "Assigning Designer"),
 		('design_center', 'Design Center'),
 		('manufacturing', "Manufacturing"),
 		('Shipping', 'Shipping'),
