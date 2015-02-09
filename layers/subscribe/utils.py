9a8679fb09f10a5ce5b4f059b65df6b3021265d9
jkol36 <jonathankolman@gmail.com>
2015-02-08 20:25:11 -0500

    added mailchimp

--- layers/subscribe/utils.py
+++ layers/subscribe/utils.py
@@ -6,6 +6,6 @@ def get_mailchimp_api():
 def add_subscriber(email=None, first_name=None, last_name=None, list_id="43eaa152c3"):
 	api = get_mailchimp_api()
 	try:
-		api.lists.list.subscribe(list_id, {'email':'%s', 'FNAME':'%s', 'LNAME':'%s'} %(email, first_name, last_name))
+		api.lists.subscribe(list_id, {'email':'%s', 'FNAME':'%s', 'LNAME':'%s'} %(email, first_name, last_name))
 	except Exception, e:
 		return e
