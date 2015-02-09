ff6cedf9df5a119d6c515da11d8e854c7d74796a
jkol36 <jonathankolman@gmail.com>
2015-02-09 14:51:32 -0500

    added mailchimp

--- layers/projects/admin.py
+++ layers/projects/admin.py
@@ -3,6 +3,8 @@ from .models import Project, Photo
 
 # Register your models here.
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+	list_display = ['title', 'client', 'budget_min', 'budget_max', 'due_date', 'project_status']
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Photo)
 
