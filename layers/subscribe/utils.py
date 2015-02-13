eb18dbc3af597ed87a6db59c2ba51f1cacb2c29d
jkol36 <jonathankolman@gmail.com>
2015-02-12 19:26:43 -0500

    working on mailchimp footer button.

--- layers/subscribe/utils.py
+++ layers/subscribe/utils.py
@@ -7,6 +7,8 @@ def add_subscriber(email=None, first_name=None, last_name=None, list_id="43eaa15
 	api = get_mailchimp_api()
 
 	try:
+		if first_name == None and last_name == None:
+			api.lists.subscribe(list_id, {'email':email, {'FNAME':email, 'LNAME':email}})
 		api.lists.subscribe(list_id, {'email':email}, {'FNAME':first_name, 'LNAME':last_name})
 	except Exception, e:
 		return e
