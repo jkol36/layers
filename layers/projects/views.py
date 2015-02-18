891b564b5df594489effd6d3638cf51c245bb9a9
jkol36 <jonathankolman@gmail.com>
2015-02-18 15:14:48 -0500

    edit_project view

--- layers/projects/views.py
+++ layers/projects/views.py
@@ -51,7 +51,7 @@ def add_photo_to_project(request, project_id=None):
 	if request.GET:
 		return render(request, 'inspiration.jade', {'project_id':project_id})
 
-	if request.POST== "POST" and request.FILES:
+	elif request.POST== "POST" and request.FILES:
 		form = add_photo_form(request.POST, request.FILES, project_id=project_id)
 		if form.is_valid():
 			#if the form is valid clear the session
