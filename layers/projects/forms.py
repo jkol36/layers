1a0dee314fdf24d4311e99c24bde1619480551b8
jkol36 <jonathankolman@gmail.com>
2015-02-17 19:08:29 -0500

    Working on Bootstrap Modals

--- layers/projects/forms.py
+++ layers/projects/forms.py
@@ -278,39 +278,8 @@ class editProject(forms.ModelForm):
 				raise forms.ValidationError('You should have a budget of at least $100.')
 
 	def save(self):
-		if not "newBudgetMin" in self.cleaned_data:
-			pass
-
-		else:
-			self.project.budget_min = self.cleaned_data['budget_min']
-			self.project.save()
-		if not "newBudgetMax" in self.cleaned_data:
-			pass
-
-		else:
-			self.project.budget_max = self.cleaned_data['budget_max']
-			self.project.save()
-
-		if not "newProjectTitle" in self.cleaned_data:
-			pass
-
-		else:
-			self.project.title = self.cleaned_data['title']
-			self.project.save()
-		if not "newProjectDescription" in self.cleaned_data:
-			pass
-
-		else:
-			self.project.description = self.cleaned_data['description']
-			self.project.save()
-
-		if not "newProjectDueData" in self.cleaned_data:
-			pass
-		else:
-			self.project.due_date = self.cleaned_data['due_date']
-			self.project.save()
-
-		return self.project
+		self.project.title = self.cleaned_data['title']
+		self.project.save()
 
 
 
