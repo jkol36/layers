873a2c59f035e6aa91a9f9a40eed460fcc0d2f02
jkol36 <jonathankolman@gmail.com>
2015-02-20 19:27:44 -0500

    Designer Submit

--- layers/projects/views.py
+++ layers/projects/views.py
@@ -101,7 +101,7 @@ def project_status(request, project_id):
 		photos = None
 
 	try:
-		if request.GET["is_designer"] == "True":
+		if request.GET["is_designer"]:
 			return render(request, 'project_status.jade', {'project':project, 'photos':photos, 'is_designer':True})
 	except Exception:
 		return render(request, 'project_status.jade', {'project':project, 'photos':photos})
